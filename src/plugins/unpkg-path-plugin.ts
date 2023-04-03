f3eb47d5201bacabdc81a197f230461a9fdab267
Filipe Sales <eduardojs999@gmail.com>
2023-04-03 07:54:01 -0400

    refactor: get input code from input area

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localForage.createInstance({
   name: "filecache",
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
@@ -38,10 +38,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === "index.js") {
           return {
             loader: "jsx",
-            contents: `
-              const message = require('react@16.0.0');
-              console.log(message);
-            `,
+            contents: inputCode,
           };
         }
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(

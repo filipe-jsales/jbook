6198dd31162a46a26e6e382e2731ec01aa24309a
Filipe Sales <eduardojs999@gmail.com>
2023-04-03 07:44:01 -0400

    fix: typescript error

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -44,19 +44,17 @@ export const unpkgPathPlugin = () => {
             `,
           };
         }
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
 
-        // Check to see if we have already fetched this file
-        // and if it is in the cache
-        const cachedResult = await fileCache.getItem(args.path);
-
-        // if it is return it immediately
         if (cachedResult) {
           return cachedResult;
         }
 
         const { data, request } = await axios.get(args.path);
 
-        const result = {
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
